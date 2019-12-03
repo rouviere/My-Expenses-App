6bf5136b2c209a986b7ec9dfbffb184616b19b74
Forrest Anderson <forrest@rouviere.com>
2019-12-03 09:07:44 -0700

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Forrest Anderson on 12/3/19.
+//  Copyright © 2019 Forrest Anderson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
